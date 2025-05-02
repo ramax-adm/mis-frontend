7bcf31c6b757456e2268b933dc071d825d8fc615
Lucas Ribeiro Santana <lucassantana00@gmail.com>
2025-05-02 08:59:16 -0300

    add storage utils and constants

--- src/utils/string.utils.ts
+++ src/utils/string.utils.ts
@@ -2,3 +2,25 @@ export function capitalizeFirstLetter(word: string) {
   if (word.length === 0) return word // Se a palavra for vazia, retorna a palavra sem alterações
   return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
 }
+
+export function parse(raw: string) {
+  return JSON.parse(raw)
+}
+
+export function safeParse(raw?: string) {
+  if (!raw) {
+    return {}
+  }
+
+  try {
+    const parsedValue = JSON.parse(raw)
+
+    return parsedValue
+  } catch {
+    return {}
+  }
+}
+
+export function stringify(raw: Object) {
+  return JSON.stringify(raw)
+}
