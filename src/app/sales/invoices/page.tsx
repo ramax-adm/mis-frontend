e4085ba9a6fee99339f5b4d392efafed80fababd
Lucas Ribeiro <lucassantana00@gmail.com>
2025-07-31 21:39:13 -0300

    wip: sales pages

---
+++ src/app/sales/invoices/page.tsx
@@ -0,0 +1,10 @@
+import { PageContainer } from "@/components/PageContainer";
+import { PageContainerHeader } from "@/components/PageContainer/header";
+
+export default function InvoicesPage() {
+  return (
+    <PageContainer>
+      <PageContainerHeader title='Vendas - Notas Fiscais' />
+    </PageContainer>
+  );
+}
