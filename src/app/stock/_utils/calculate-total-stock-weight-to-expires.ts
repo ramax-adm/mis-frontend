a1dc4875883e3e08a1501534054bb86edb1ade24
Lucas Ribeiro <lucassantana00@gmail.com>
2025-07-31 22:37:50 -0300

    feat: add a total expired weight in to expires card

--- src/app/stock/_utils/calculate-total-stock-weight-to-expires.ts
+++ src/app/stock/_utils/calculate-total-stock-weight-to-expires.ts
@@ -1,23 +1,24 @@
 import {
   GetAnalyticalToExpiresByCompanyResponse,
   GetToExpiresByCompanyResponse,
-} from '@/types/api/stock'
-import { fromLocaleStringToNumber, toLocaleString } from '@/utils/number.utils'
+} from "@/types/api/stock";
+import { fromLocaleStringToNumber, toLocaleString } from "@/utils/number.utils";
 
 export function calculateTotalStockWeightToExpires(
-  data: GetToExpiresByCompanyResponse[] | GetAnalyticalToExpiresByCompanyResponse[],
-  daysQuantityA: number = 0,
-  daysQuantityB: number = 0,
+  data:
+    | GetToExpiresByCompanyResponse[]
+    | GetAnalyticalToExpiresByCompanyResponse[],
+  daysQuantityA: number,
+  daysQuantityB: number
 ) {
   const result = data
-    .filter((item) =>
-      daysQuantityA || daysQuantityA !== 0 ? item.daysToExpires >= daysQuantityA : true,
-    )
-    .filter((item) =>
-      daysQuantityB || daysQuantityB !== 0 ? item.daysToExpires <= daysQuantityB : true,
-    )
-    .map((item) => ({ ...item, totalWeightInKg: fromLocaleStringToNumber(item.totalWeightInKg) }))
-    .reduce((acc, item) => acc + item.totalWeightInKg, 0)
+    .filter((item) => item.daysToExpires >= daysQuantityA)
+    .filter((item) => item.daysToExpires <= daysQuantityB)
+    .map((item) => ({
+      ...item,
+      totalWeightInKg: fromLocaleStringToNumber(item.totalWeightInKg),
+    }))
+    .reduce((acc, item) => acc + item.totalWeightInKg, 0);
 
-  return toLocaleString(result)
+  return toLocaleString(result);
 }
