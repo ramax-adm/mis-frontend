e77b9ff3e0509aa53aea2cf0b274ec0886ac1a74
Lucas Ribeiro <lucassantana00@gmail.com>
2025-08-07 13:23:27 -0300

    improvement: fix pre-filters when navigating between business audit and sales invoices page

--- src/services/react-query/queries/business-audit.ts
+++ src/services/react-query/queries/business-audit.ts
@@ -1,6 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { queryKeys } from "../query-keys";
-import { GetBusinessAuditResumeData } from "@/services/webApi/business-audit-api";
+import {
+  GetBusinessAuditConsideredCfops,
+  GetBusinessAuditConsideredNfSituations,
+  GetBusinessAuditResumeData,
+} from "@/services/webApi/business-audit-api";
 import { GetBusinessAuditResumeDataResponse } from "@/types/api/business-audit";
 
 export const useGetBusinessAuditResumeData = ({
@@ -18,6 +22,28 @@ export const useGetBusinessAuditResumeData = ({
     ],
     queryFn: async () => {
       const response = await GetBusinessAuditResumeData({ startDate, endDate });
+
+      return response;
+    },
+  });
+};
+
+export const useGetBusinessAuditConsideredCfops = () => {
+  return useQuery<string[]>({
+    queryKey: [queryKeys.BUSINESS_AUDIT.GET_CONSIDERED_CFOPS],
+    queryFn: async () => {
+      const response = await GetBusinessAuditConsideredCfops();
+
+      return response;
+    },
+  });
+};
+
+export const useGetBusinessAuditConsideredNfSituations = () => {
+  return useQuery<string[]>({
+    queryKey: [queryKeys.BUSINESS_AUDIT.GET_CONSIDERED_NF_SITUATIONS],
+    queryFn: async () => {
+      const response = await GetBusinessAuditConsideredNfSituations();
 
       return response;
     },
--- src/services/webApi/business-audit-api.ts
+++ src/services/webApi/business-audit-api.ts
@@ -16,3 +16,17 @@ export async function GetBusinessAuditResumeData({
 
   return response.data;
 }
+
+export async function GetBusinessAuditConsideredCfops() {
+  const response = await GetFetch(urls.BUSINESS_AUDIT.GET_CONSIDERED_CFOPS);
+
+  return response.data;
+}
+
+export async function GetBusinessAuditConsideredNfSituations() {
+  const response = await GetFetch(
+    urls.BUSINESS_AUDIT.GET_CONSIDERED_NF_SITUATIONS
+  );
+
+  return response.data;
+}
