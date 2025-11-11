218c3879b11b9f0d441c436a97360097c94b24fa
Lucas Ribeiro <lucassantana00@gmail.com>
2025-11-11 16:21:35 -0300

    fix: ux adjusts for menu subitems and card styles

--- src/components/Card/title.tsx
+++ src/components/Card/title.tsx
@@ -1,14 +1,19 @@
-import { Box, BoxProps, Typography, TypographyProps } from '@mui/material'
+import { Box, BoxProps, Typography, TypographyProps } from "@mui/material";
 
 interface CardTitleProps extends BoxProps {
-  titleProps?: TypographyProps
+  titleProps?: TypographyProps;
 }
 export function CardTitle({ children, ...props }: CardTitleProps) {
   return (
-    <Box sx={{ width: '100%', ...props?.sx }} {...props}>
-      <Typography variant='body2' fontWeight={700} color={'#3E63DD'} {...props.titleProps}>
+    <Box sx={{ width: "100%", ...props?.sx }} {...props}>
+      <Typography
+        fontSize={"12px"}
+        fontWeight={700}
+        color={"#3E63DD"}
+        {...props.titleProps}
+      >
         {children}
       </Typography>
     </Box>
-  )
+  );
 }
