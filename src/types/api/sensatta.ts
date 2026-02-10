fb86db78389adb8d64d5dff1c8516e04257d4325
Lucas Ribeiro <lucassantana00@gmail.com>
2026-02-10 14:58:45 -0300

    update 10.02.26

--- src/types/api/sensatta.ts
+++ src/types/api/sensatta.ts
@@ -1,36 +1,37 @@
 export interface GetProductClassificationTypesResponse {
-  label: string
-  key: string
+  label: string;
+  key: string;
 }
 
 export interface Company {
-  id: string
-  sensattaCode: string
-  city: string
-  uf: string
-  name: string
-  fantasyName: string
-  isConsideredOnStock: boolean
-  createdAt: Date
+  id: string;
+  sensattaCode: string;
+  city: string;
+  uf: string;
+  name: string;
+  fantasyName: string;
+  isConsideredOnStock: boolean;
+  createdAt: Date;
 }
 
 export interface Product {
-  id: string
-  sensattaId: string
-  sensattaCode: string
-  name: string
-  productLineId: string
-  unitCode: string
-  classificationType: string
-  createdAt: Date
+  id: string;
+  sensattaId: string;
+  sensattaCode: string;
+  name: string;
+  productLineId: string;
+  unitCode: string;
+  classificationType: string;
+  createdAt: Date;
 }
 
 export interface ProductLine {
-  id: string
-  sensattaId: string // sequencial linha
-  sensattaCode: string // codigo linha
-  name: string // descricao
-  acronym: string // sigla
-  isConsideredOnStock: boolean
-  createdAt: Date
+  id: string;
+  sensattaId: string; // sequencial linha
+  sensattaCode: string; // codigo linha
+  name: string; // descricao
+  acronym: string; // sigla
+  isConsideredOnStock: boolean;
+  market: string;
+  createdAt: Date;
 }
